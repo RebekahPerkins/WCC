bad45c853485839d03f9fec4befa6f72099eb4a8
RebekahPerkins <r.perkins.smith@gmail.com>
2016-11-13 16:51:57 -0800

    input, missed checkin earlier

---
+++ mosthighleveldir/input.py
@@ -0,0 +1,5 @@
+#name = raw_input('What is your name?')
+#print('Hi ' + name)
+age = raw_input('How old are you?')
+dog_age = int(age) * 7
+print('You are ' + str(dog_age) + ' old in dog years!\nThat\'s old!')
