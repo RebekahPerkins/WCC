21df006dca9e8dc10ae8074add5d6eb18af00cff
RebekahPerkins <r.perkins.smith@gmail.com>
2016-11-13 16:51:43 -0800

    tip calculator program

---
+++ mosthighleveldir/tip_calculator.py
@@ -0,0 +1,8 @@
+meal_cost = raw_input('How much was your meal?')
+#45
+tip = float(meal_cost) *.20
+total_cost = float(meal_cost) * 1.20 
+print('You should tip $' + str(tip))
+#You should tip $9.0
+print('Your total cost would be $' + str(total_cost))
+#Your total cost would be $54.0
